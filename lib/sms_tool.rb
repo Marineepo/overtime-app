96e9e302d88940a431cc3da3134b25d542544847
Jordan Hudgens <jordan@devcamp.com>
2017-05-24 14:06:00 +0000

    integreated live connect for twilio

--- lib/sms_tool.rb
+++ lib/sms_tool.rb
@@ -1,6 +1,17 @@
 module SmsTool
+	account_sid = ENV['TWILIO_ACCOUNT_SID']
+	auth_token = ENV['TWILIO_ACCOUNT_SID']
+	
+	@client = Twilio::REST::Client.new account_sid, auth_token
+	
 	def self.send_sms(number:, message:)
-		puts "Sending SMS..."
-		puts "#{message} to #{number}"
+		@client.message.create(
+			from: ENV[TWILIO_PHONE_NUMBER],
+			to: "+1#{number}",
+			body: "#{message}"
+		)
 	end
 end
+
+# SmsTool.send_sms(number: '5555555555', message: "Look at me I am in a text!")
+# do rails c and then pass in all of line 16 once you insert token and SID
