ba3c8b99a143db3d628901a54d4046720e9807b8
Jordan Hudgens <jordan@devcamp.com>
2017-05-24 20:48:29 +0000

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,29 @@
-@user = User.create(email: "test@test.com", 
-                    password: "asdfasdf", 
-                    password_confirmation: "asdfasdf", 
-                    first_name: "Jon", 
-                    last_name: "Snow",
-                    phone: "55555555555")
+@user = User.create(email: "test@test.com",
+										password: "asdfasdf",
+										password_confirmation: "asdfasdf",
+										first_name: "Jon",
+										last_name: "Snow",
+										phone: "4322386131")
+
 puts "1 User created"
 
-AdminUser.create(email: "Marineepo@gmail.com", 
-                 password: "Buckeyefan1987$", 
-                 password_confirmation: "Buckeyefan1987$", 
-                 first_name: "Benjamin", 
-                 last_name: "Nicklaus",
-                 phone: "12393510443")
+AdminUser.create(email: "admin@test.com",
+									password: "asdfasdf",
+									password_confirmation: "asdfasdf",
+									first_name: "Admin",
+									last_name: "Name",
+									phone: "4322386131")
 
-puts "1 Admin created"
+puts "1 Admin User created"
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 
-puts "100 posts have been created"
+puts "100 Posts have been created"
 
 100.times do |audit_log|
-  AuditLog.create!(user_id: User.last.id, status: 0, start_date: Date.today)
+	AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
 end
 
 puts "100 audit logs have been created"
