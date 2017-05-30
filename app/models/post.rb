781bad787b4484b7ee901d561526293476c86ae2
Jordan Hudgens <jordan@devcamp.com>
2017-05-30 17:33:33 +0000

    implemeneted nil gaurd for callback

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,7 +13,8 @@ class Post < ApplicationRecord
   
   def update_audit_log
     audit_log = AuditLog.where(user_id: self.user_id, start_date: (self.date - 7.days..self.date)).last
-    audit_log.confirmed!
+    audit_log.confirmed! if audit_log
+    #if statment is a nil guard
   end
 end
 
